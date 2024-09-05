d4c566504e812a60b274f8b9caf506995e8a2e8c
Gary-love <sigmapet086@gmail.com>
2024-09-05 12:00:45 +0300

    Add buttton to add Add 'Add Book' and 'Remove Book' buttons

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let btnAdd =document.querySelector("button")
+let table = document.querySelector("table")
+let nameInput=document.querySelector("#name")
+let authorInput=document.querySelector("#author")
+let pagesInput=document.querySelector("#pages")
+let statusInput=document.querySelector("#status")
+let id=1;
+function removeBook(e){
+    id-=1;
+     if(!e.target.classList.contains("delBtn")){
+        return;
+     }
+     const btn=e.target
+     btn.closest("tr").remove()
+     let bookToRemove=myLibrary.find(book=>book.name===btn.closest("tr").children[1].textContent)
+     myLibrary.splice(myLibrary.indexOf(bookToRemove),1)
+}
+table.addEventListener("click",removeBook)
 const myLibrary=[]
 function Book(name,author,pages,status){
     this.name=name;
@@ -5,12 +23,26 @@ function Book(name,author,pages,status){
     this.pages=pages;
     this.status=status;
 }
-function addBookToLibrary(){
-    let name=prompt("Enter book name:");
-    let author=prompt("Enter book author:");
-    let pages=parseInt(prompt("Enter book pages:"));
-    let status=prompt("Enter book status (read/unread):");
+function addBookToLibrary(name,author,pages,status){
     let newBook=new Book(name,author,pages,status);
     myLibrary.push(newBook);
-    console.log("Book added successfully");
+    console.log("Book added successfully"); 
 }
+btnAdd.addEventListener("click", ()=>{
+    id+=1
+    let name=nameInput.value;
+    let author=authorInput.value;
+    let pages=parseInt(pagesInput.value);
+    let status=statusInput.value;
+    let template =
+    `<tr bgcolor="lightgrey" align="center">
+        <td>${id}</td>
+        <td>${name}</td>
+        <td>${author}</td>
+        <td>${pages}</td>
+        <td>${status}</td>
+        <td><button class="delBtn">Remove</button></td>`
+        table.innerHTML+=template
+        addBookToLibrary(name,author,pages,status)
+        console.log(myLibrary)
+})
